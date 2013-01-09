1e001d9eb96b61b913ebe23f58ff54febcf1e0b5
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-01-09 14:26:13 -0800

    parallel accumulate groupBy

--- src/anagrams/Anagrams.java
+++ src/anagrams/Anagrams.java
@@ -6,27 +6,26 @@ import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
+import java.util.function.Function;
+import static java.util.stream.Accumulators.groupBy;
 
 public class Anagrams {
 
     public static void main(String[] args) throws IOException {
         assert args.length == 1; // words path
-        Map<String, List<String>> map = new HashMap<>();
         Path path = FileSystems.getDefault().getPath(args[0]);
+        Function<String, String> key = s -> {
+            char[] chars = s.toLowerCase().toCharArray();
+            Arrays.sort(chars);
+            return new String(chars);
+        };
+        final Map<String, Collection<String>> map;
         try (BufferedReader r = Files.newBufferedReader(path, Charset.defaultCharset())) {
-            r.lines().forEach(s -> map.computeIfAbsent(key(s), k -> new ArrayList<>()).add(s));
+            map = r.lines().parallel().accumulate(groupBy(key));
         }
-        map.values().stream().filter(v -> v.size() > 1).forEach(v -> System.out.println(v));
-    }
-
-    private static String key(String s) {
-        char[] chars = s.toLowerCase().toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
+        map.values().stream().filter(v -> v.size() > 1).forEach(System.out::println);
     }
 }
