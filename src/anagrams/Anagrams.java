6189a2c12eb207150f6ebf070917e6cf84efa9e3
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-01-10 09:57:21 -0800

    Anagrams::key

--- src/anagrams/Anagrams.java
+++ src/anagrams/Anagrams.java
@@ -9,23 +9,23 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
-import java.util.function.Function;
-import static java.util.stream.Accumulators.groupBy;
+import java.util.stream.Accumulators;
 
 public class Anagrams {
 
     public static void main(String[] args) throws IOException {
         assert args.length == 1; // words path
         Path path = FileSystems.getDefault().getPath(args[0]);
-        Function<String, String> key = s -> {
-            char[] chars = s.toLowerCase().toCharArray();
-            Arrays.sort(chars);
-            return new String(chars);
-        };
         final Map<String, Collection<String>> map;
         try (BufferedReader r = Files.newBufferedReader(path, Charset.defaultCharset())) {
-            map = r.lines().parallel().accumulate(groupBy(key));
+            map = r.lines().accumulate(Accumulators.<String, String>groupBy(Anagrams::key));
         }
         map.values().stream().filter(v -> v.size() > 1).forEach(System.out::println);
+    }
+
+    private static String key(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
