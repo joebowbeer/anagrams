b5baf51af31592072e75f6f1b11cab729bc218de
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-01-13 23:24:08 -0800

    Add mostLetters and mostWords comparators.

--- src/anagrams/Anagrams.java
+++ src/anagrams/Anagrams.java
@@ -8,24 +8,56 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Accumulators;
+import java.util.stream.Stream;
 
 public class Anagrams {
 
     public static void main(String[] args) throws IOException {
         assert args.length == 1; // words path
         Path path = FileSystems.getDefault().getPath(args[0]);
-        final Map<String, Collection<String>> map;
         try (BufferedReader r = Files.newBufferedReader(path, Charset.defaultCharset())) {
-            map = r.lines().accumulate(Accumulators.<String, String>groupBy(Anagrams::key));
+            anagrams(r.lines()).forEach(System.out::println);
         }
-        map.values().stream().filter(v -> v.size() > 1).forEach(System.out::println);
     }
+
+    public static Stream<Collection<String>> anagrams(Stream<String> words) {
+        Map<String, Collection<String>> map = words.accumulate(
+                Accumulators.<String, String>groupBy(Anagrams::key));
+        return map.values().parallelStream().filter(v -> v.size() > 1);
+    } 
 
     private static String key(String s) {
         char[] chars = s.toLowerCase().toCharArray();
         Arrays.sort(chars);
         return new String(chars);
+    }
+
+    public static Collection<String> anagramWithMostLetters(Stream<String> words) {
+        return anagrams(words).max(mostLetters).orElse(() -> null);
+    }
+
+    public static Collection<String> anagramWithMostWords(Stream<String> words) {
+        return anagrams(words).max(mostWords).orElse(() -> null);
+    }
+
+    private static Comparator<Collection<String>> mostLetters = new MostLettersComparator();
+
+    private static Comparator<Collection<String>> mostWords = new MostWordsComparator();
+
+    private static class MostLettersComparator implements Comparator<Collection<String>> {
+        @Override
+        public int compare(Collection<String> o1, Collection<String> o2) {
+            return o1.iterator().next().length() - o2.iterator().next().length();
+        }
+    }
+
+    private static class MostWordsComparator implements Comparator<Collection<String>> {
+        @Override
+        public int compare(Collection<String> o1, Collection<String> o2) {
+            return o1.size() - o2.size();
+        }
     }
 }
