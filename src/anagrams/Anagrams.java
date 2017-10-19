55ed0b504362d929fa939069af9d79d08d6483cd
Joe Bowbeer <joe.bowbeer@gmail.com>
2017-10-19 02:58:10 -0700

    codePoints

--- src/anagrams/Anagrams.java
+++ src/anagrams/Anagrams.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import static java.util.Comparator.comparingInt;
@@ -14,43 +13,42 @@ import java.util.stream.Stream;
 
 public class Anagrams {
 
-    public static void main(String[] args) throws IOException {
-        assert args.length == 1; // path-to-words
-        Path path = FileSystems.getDefault().getPath(args[0]);
-        anagrams(Files.lines(path)).forEach(System.out::println);
-    }
+  public static void main(String[] args) throws IOException {
+    assert args.length == 1; // path-to-words
+    Path path = FileSystems.getDefault().getPath(args[0]);
+    anagrams(Files.lines(path)).forEach(System.out::println);
+  }
 
-    public static Stream<List<String>> anagrams(Stream<String> words) {
-        return words.parallel().collect(groupingByConcurrent(Anagrams::key))
-                .values().parallelStream().filter(v -> v.size() > 1);
-    } 
+  public static Stream<List<String>> anagrams(Stream<String> words) {
+    return words.parallel().collect(groupingByConcurrent(Anagrams::key))
+        .values().parallelStream().filter(v -> v.size() > 1);
+  }
 
-    public static Stream<String> anagramsFor(String word, Stream<String> words) {
-        String keyWord = key(word);
-        return words.parallel().filter(s -> key(s).equals(keyWord) && !s.equals(word));
-    }
+  public static Stream<String> anagramsFor(String word, Stream<String> words) {
+    String keyWord = key(word);
+    return words.parallel().filter(s -> key(s).equals(keyWord) && !s.equals(word));
+  }
 
-    public static String anyAnagramFor(String word, Stream<String> words) {
-        return anagramsFor(word, words).findAny().orElse(null);
-    }
+  public static String anyAnagramFor(String word, Stream<String> words) {
+    return anagramsFor(word, words).findAny().orElse(null);
+  }
 
-    private static String key(String s) {
-        char[] chars = s.toLowerCase().toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
-    }
+  private static String key(String s) {
+    int[] codePoints = s.codePoints().sorted().toArray();
+    return new String(codePoints, 0, codePoints.length);
+  }
 
-    public static Collection<String> anagramWithMostLetters(Stream<String> words) {
-        return anagrams(words).max(mostLetters).orElse(null);
-    }
+  public static Collection<String> anagramWithMostLetters(Stream<String> words) {
+    return anagrams(words).max(mostLetters).orElse(null);
+  }
 
-    public static Collection<String> anagramWithMostWords(Stream<String> words) {
-        return anagrams(words).max(mostWords).orElse(null);
-    }
+  public static Collection<String> anagramWithMostWords(Stream<String> words) {
+    return anagrams(words).max(mostWords).orElse(null);
+  }
 
-    private static final Comparator<Collection<String>> mostLetters =
-        comparingInt(cs -> cs.iterator().next().length());
+  private static final Comparator<Collection<String>> mostLetters
+      = comparingInt(cs -> cs.iterator().next().length());
 
-    private static final Comparator<Collection<String>> mostWords =
-        comparingInt(Collection::size);
+  private static final Comparator<Collection<String>> mostWords
+      = comparingInt(Collection::size);
 }
--- test/anagrams/AnagramsTest.java
+++ test/anagrams/AnagramsTest.java
@@ -3,117 +3,113 @@ package anagrams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
-
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.Test;
-
-import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertThat;
+import org.junit.Test;
 
 public class AnagramsTest {
 
-    /**
-     * Test of {@link Anagrams#anagrams(Stream) anagrams} method.
-     */
-    @Test
-    public void testAnagrams() {
-        Stream<String> words = Arrays.asList("risen", "siren").stream();
-        Collection<String> actual = Anagrams.anagrams(words).findFirst().get();
-        assertThat(actual, containsInAnyOrder("risen", "siren"));
-    }
+  /**
+   * Test of {@link Anagrams#anagrams(Stream) anagrams} method.
+   */
+  @Test
+  public void testAnagrams() {
+    Stream<String> words = Arrays.asList("risen", "siren").stream();
+    Collection<String> actual = Anagrams.anagrams(words).findFirst().get();
+    assertThat(actual, containsInAnyOrder("risen", "siren"));
+  }
 
-    /**
-     * Test of {@link Anagrams#anagramsFor(String, Stream) anagramsFor} method.
-     */
-    @Test
-    public void testAnagramsFor() {
-        Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
-                "siren", "steer", "terse", "trees", "unsorted");
-        Stream<String> actual = Anagrams.anagramsFor("trees", words.stream());
-        assertThat(actual.collect(toList()), containsInAnyOrder("reset", "steer", "terse"));
-    }
+  /**
+   * Test of {@link Anagrams#anagramsFor(String, Stream) anagramsFor} method.
+   */
+  @Test
+  public void testAnagramsFor() {
+    Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
+        "siren", "steer", "terse", "trees", "unsorted");
+    Stream<String> actual = Anagrams.anagramsFor("trees", words.stream());
+    assertThat(actual.collect(toList()), containsInAnyOrder("reset", "steer", "terse"));
+  }
 
-    /**
-     * Test of {@link Anagrams#anyAnagramFor(String, Stream) anyAnagramFor} method.
-     */
-    @Test
-    public void testAnyAnagramFor() {
-        Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
-                "siren", "steer", "terse", "trees", "unsorted");
-        String actual = Anagrams.anyAnagramFor("trees", words.stream());
-        assertThat(actual, isOneOf("reset", "steer", "terse"));
-    }
+  /**
+   * Test of {@link Anagrams#anyAnagramFor(String, Stream) anyAnagramFor} method.
+   */
+  @Test
+  public void testAnyAnagramFor() {
+    Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
+        "siren", "steer", "terse", "trees", "unsorted");
+    String actual = Anagrams.anyAnagramFor("trees", words.stream());
+    assertThat(actual, isOneOf("reset", "steer", "terse"));
+  }
 
-    /**
-     * Test of
-     * {@link Anagrams#anagramWithMostLetters(Stream) anagramWithMostLetters}
-     * method.
-     */
-    @Test
-    public void testAnagramWithMostLetters() {
-        Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
-                "siren", "steer", "terse", "trees", "unsorted");
-        Collection<String> actual = Anagrams.anagramWithMostLetters(words.stream());
-        assertThat(actual, containsInAnyOrder("catechism", "schematic"));
-    }
+  /**
+   * Test of {@link Anagrams#anagramWithMostLetters(Stream) anagramWithMostLetters} method.
+   */
+  @Test
+  public void testAnagramWithMostLetters() {
+    Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
+        "siren", "steer", "terse", "trees", "unsorted");
+    Collection<String> actual = Anagrams.anagramWithMostLetters(words.stream());
+    assertThat(actual, containsInAnyOrder("catechism", "schematic"));
+  }
 
-    /**
-     * Test of
-     * {@link Anagrams#anagramWithMostWords(Stream) anagramWithMostWords}
-     * method.
-     */
-    @Test
-    public void testAnagramWithMostWords() {
-        Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
-                "siren", "steer", "terse", "trees", "unsorted");
-        Collection<String> actual = Anagrams.anagramWithMostWords(words.stream());
-        assertThat(actual, containsInAnyOrder("reset", "steer", "terse", "trees"));
-    }
+  /**
+   * Test of {@link Anagrams#anagramWithMostWords(Stream) anagramWithMostWords} method.
+   */
+  @Test
+  public void testAnagramWithMostWords() {
+    Collection<String> words = Arrays.asList("catechism", "reset", "risen", "schematic",
+        "siren", "steer", "terse", "trees", "unsorted");
+    Collection<String> actual = Anagrams.anagramWithMostWords(words.stream());
+    assertThat(actual, containsInAnyOrder("reset", "steer", "terse", "trees"));
+  }
 
-    @SafeVarargs
-    static <T> Matcher<Iterable<? extends T>> containsInAnyOrder(final T... items) {
-        return new BaseMatcher<Iterable<? extends T>>() {
-            @Override
-            public boolean matches(Object item) {
-                Collection<T> expected = Arrays.asList(items);
-                @SuppressWarnings("unchecked")
-                Collection<T> actual = toCollection((Iterable<T>) item);
-                return actual.size() == expected.size() && actual.containsAll(expected);
-            }
-            @Override
-            public void describeTo(Description description) {
-                description.appendText(Arrays.asList(items) + " in any order");
-            }
-        };
-    }
+  @SafeVarargs
+  static <T> Matcher<Iterable<? extends T>> containsInAnyOrder(final T... items) {
+    return new BaseMatcher<Iterable<? extends T>>() {
+      @Override
+      public boolean matches(Object item) {
+        Collection<T> expected = Arrays.asList(items);
+        @SuppressWarnings("unchecked")
+        Collection<T> actual = toCollection((Iterable<T>) item);
+        return actual.size() == expected.size() && actual.containsAll(expected);
+      }
 
-    @SafeVarargs
-    static <T> Matcher<T> isOneOf(final T... items) {
-        return new TypeSafeMatcher<T>() {
-            @Override
-            public boolean matchesSafely(T item) {
-                return Arrays.asList(items).contains(item);
-            }
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("one of " + Arrays.asList(items));
-            }
-        };
-    }
+      @Override
+      public void describeTo(Description description) {
+        description.appendText(Arrays.asList(items) + " in any order");
+      }
+    };
+  }
 
-    @SuppressWarnings("unchecked")
-    static <T> Collection<T> toCollection(Iterable<T> iter) {
-        if (iter instanceof Collection) {
-            return (Collection<T>) iter;
-        }
-        Collection<T> list = new ArrayList<>();
-        for (T item: iter) {
-            list.add(item);
-        }
-        return list;
+  @SafeVarargs
+  static <T> Matcher<T> isOneOf(final T... items) {
+    return new TypeSafeMatcher<T>() {
+      @Override
+      public boolean matchesSafely(T item) {
+        return Arrays.asList(items).contains(item);
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("one of " + Arrays.asList(items));
+      }
+    };
+  }
+
+  @SuppressWarnings("unchecked")
+  static <T> Collection<T> toCollection(Iterable<T> iter) {
+    if (iter instanceof Collection) {
+      return (Collection<T>) iter;
     }
+    Collection<T> list = new ArrayList<>();
+    for (T item : iter) {
+      list.add(item);
+    }
+    return list;
+  }
 }
